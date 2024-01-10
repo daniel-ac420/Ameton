e840d16d1b51f8885bff705d7dba34f313e58b3e
daniel-ac420 <67051360+daniel-ac420@users.noreply.github.com>
2024-01-11 01:29:49 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,27 @@
+﻿const swiper = new Swiper(".faq-box", {
+	direction: "horizontal",
+	loop: true,
+	spaceBetween: 24,
+	slidesPerView: 4,
+
+	// Navigation arrows
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+	
+	breakpoints: {
+		1181: {
+			slidesPerView: 4
+		},
+		1024: {
+			slidesPerView: 3
+		},
+		768: {
+			slidesPerView: 2
+		},
+		320: {
+			slidesPerView: 1
+		}
+	}
+});
